70b346b116dfcc17b58a7e6a183cb44b7820dd36
Ben Wootten <bwootten@udel.edu>
2025-03-31 10:55:09 -0400

    initialized MultipleChoiceQuestion component

---
+++ src/basic-quiz-components/MultipleChoiceQuestion.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+export function MultipleChoiceQuestion(): React.JSX.Element {
+    return (
+        <div>
+            Question
+        </div>
+    );
+}
