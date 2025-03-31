214f964c22a49f494ebfe80ee113c07c3a93d037
Thomas Glenn <tglenn@udel.edu>
2025-03-31 19:46:19 -0400

    created outline for detailed assessment page

---
+++ src/DetailedAssessment.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function DetailedAssessment(): React.JSX.Element {
+    return (
+        <div>
+          <h1>Detailed Career Assessment</h1>
+          <p>Detailed Questions</p>
+        </div>
+    )
+}
