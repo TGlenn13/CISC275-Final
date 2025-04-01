b37813591f81d6f464be3ffe573e3a998d5dca72
Ben Wootten <bwootten@udel.edu>
2025-04-01 18:12:55 -0400

    implemented radio buttons

--- src/basic-quiz-components/MultipleChoiceQuestion.tsx
+++ src/basic-quiz-components/MultipleChoiceQuestion.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap"
 
 export function MultipleChoiceQuestion({question, options}: {question: string, options: string[]}): React.JSX.Element {
+    const [chosenOption, setChosenOption] = useState<string>("");
     return (
         <div>
             {question}
             {options.map((option: string): React.JSX.Element => {
-                return <div>{option}</div>
+                return <Form.Check
+                    type="radio"
+                    name={question}
+                    onChange={() => {
+                        setChosenOption(option);
+                    }}
+                    id={"option-" + option}
+                    value={option}
+                    checked={chosenOption === option}
+                    label={option}
+                />
             })}
         </div>
     );
