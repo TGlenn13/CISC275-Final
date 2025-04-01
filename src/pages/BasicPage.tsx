da1567d3afbcf1e8ce20751b1ea60ffe43229125
Ben Wootten <bwootten@udel.edu>
2025-04-01 18:45:58 -0400

    set up BasicPage with placeholder MultipleChoiceQuestions

--- src/pages/BasicPage.tsx
+++ src/pages/BasicPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { MultipleChoiceQuestion } from "../basic-quiz-components/MultipleChoiceQuestion";
 
 interface BasicPageProps{
     changePage: (pageName: "home" | "basic") => void;
@@ -8,6 +10,18 @@ interface BasicPageProps{
 export function BasicPage({changePage}:BasicPageProps): React.JSX.Element{
     return(
     <div>
+        <div>
+            <Row>
+                <Col>
+                    <MultipleChoiceQuestion question={"Question 1"} options={["Answer 1","Answer 2","Answer 3","Answer 4"]}></MultipleChoiceQuestion>
+                    <MultipleChoiceQuestion question={"Question 3"} options={["Answer 1","Answer 2","Answer 3"]}></MultipleChoiceQuestion>
+                </Col>
+                <Col>
+                    <MultipleChoiceQuestion question={"Question 2"} options={["Answer 1","Answer 2","Answer 3","Answer 4"]}></MultipleChoiceQuestion>
+                    <MultipleChoiceQuestion question={"Question 4"} options={["Answer 1","Answer 2","Answer 3","Answer 4"]}></MultipleChoiceQuestion>
+                </Col>
+            </Row>
+        </div>
         <Button onClick={() => changePage("home")}>go back</Button>
     </div>
     )
