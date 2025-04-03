bb1cf53435033dc2f503198743df792501288eaf
Ben Wootten <bwootten@udel.edu>
2025-04-03 15:17:28 -0400

    added question content

--- src/pages/BasicPage.tsx
+++ src/pages/BasicPage.tsx
@@ -10,15 +10,94 @@ interface BasicPageProps{
 export function BasicPage({changePage}:BasicPageProps): React.JSX.Element{
     return(
     <div>
+        <h1>Basic Career Assessment</h1>
         <div>
             <Row>
                 <Col>
-                    <MultipleChoiceQuestion question={"Question 1"} options={["Answer 1","Answer 2","Answer 3","Answer 4"]}></MultipleChoiceQuestion>
-                    <MultipleChoiceQuestion question={"Question 3"} options={["Answer 1","Answer 2","Answer 3"]}></MultipleChoiceQuestion>
+                    <MultipleChoiceQuestion
+                        question={"1. Which activity sounds the most enjoyable to you?"}
+                        options={[
+                            "Solving puzzles or analyzing data",
+                            "Creating art, music, or writing stories",
+                            "Helping people solve personal or emotional problems",
+                            "Building or repairing things with your hands",
+                            "Leading a team or organizing an event"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
+                    <MultipleChoiceQuestion 
+                        question={"3. What type of environment do you prefer to work in?"}
+                        options={[
+                            "Quiet, focused, and independent",
+                            "Dynamic, expressive, and collaborative",
+                            "Supportive and empathetic",
+                            "Hands-on, physical, and practical",
+                            "Fast-paced, strategic, and goal-oriented"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
+                    <MultipleChoiceQuestion 
+                        question={"5. Which school subject did you (or do you) enjoy the most?"}
+                        options={[
+                            "Math or Science",
+                            "Art, Literature, or Music",
+                            "Psychology or Social Studies",
+                            "Woodshop, Engineering, or Home Economics",
+                            "Business or Economics"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
+                    <MultipleChoiceQuestion
+                        question={"7. What motivates you most in a career?"}
+                        options={[
+                            "Solving complex problems and discovering new things",
+                            "Expressing myself and inspiring others",
+                            "Making a positive difference in people’s lives",
+                            "Creating tangible results with my work",
+                            "Achieving success and recognition"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
                 </Col>
+
+
                 <Col>
-                    <MultipleChoiceQuestion question={"Question 2"} options={["Answer 1","Answer 2","Answer 3","Answer 4"]}></MultipleChoiceQuestion>
-                    <MultipleChoiceQuestion question={"Question 4"} options={["Answer 1","Answer 2","Answer 3","Answer 4"]}></MultipleChoiceQuestion>
+                <MultipleChoiceQuestion 
+                        question={"2. How do you typically make decisions?"}
+                        options={[
+                            "Based on logic, facts, and data",
+                            "Through creativity and intuition",
+                            "By considering others' feelings and needs",
+                            "From experience and practicality",
+                            "Strategically and with long-term goals in mind"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
+                    <MultipleChoiceQuestion 
+                        question={"4. What kind of tasks do you find most satisfying?"}
+                        options={[
+                            "Researching, investigating, or experimenting",
+                            "Designing, writing, or performing",
+                            "Listening, counseling, or caring for others",
+                            "Assembling, fixing, or operating tools or machinery",
+                            "Planning, negotiating, or managing"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
+                    <MultipleChoiceQuestion 
+                        question={"6. How would your friends describe you?"}
+                        options={[
+                            "Analytical and thoughtful",
+                            "Imaginative and expressive",
+                            "Empathetic and supportive",
+                            "Resourceful and hands-on",
+                            "Ambitious and persuasive"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
+                    <MultipleChoiceQuestion 
+                        question={"8. What do you value most in a job?"}
+                        options={[
+                            "Intellectual challenge",
+                            "Creative freedom",
+                            "Human connection",
+                            "Practical skills and independence",
+                            "Financial success and leadership opportunities"
+                            ]}></MultipleChoiceQuestion>
+                    <br></br>
                 </Col>
             </Row>
         </div>
