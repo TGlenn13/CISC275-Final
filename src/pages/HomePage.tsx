a48d4451ff67c356ae3e4000afd88ec856ec4827
Ben Wootten <bwootten@udel.edu>
2025-04-04 22:10:07 -0400

    added descriptions for simple and detailed career quizzes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../App.css';
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import "./HomePage.css"
 
 interface HomePageProps {
   changePage: (pageName: "home" | "basic" | "detailed") => void;
@@ -13,7 +14,29 @@ export function HomePage({ changePage, changeKey, handleSubmit, keyValue }: Home
   return (
     <div className="App">
       <h1>Career Helpi</h1>
-      <Form>
+      <div className="row-container">
+      <Row>
+        <Col>
+          <div className="button-container">
+            <p>The basic career assessment contains simple multiple choice questions and takes just
+              a few minutes to complete. Great for people looking for a quick push in the right direction
+              to finding their future careers!
+            </p>
+            <Button className="button" onClick={() => changePage("basic")}>Basic Career Assessment</Button>
+          </div>
+        </Col>
+        <Col>
+          <div className="button-container">
+            <p>The detailed career assessment contains more in-depth short answer questions that
+              may require some serious thinking. This quiz can take around 15 minutes to properly complete,
+              but will give far more detailed results about what career is right for you!
+            </p>
+            <Button className="button" onClick={() => changePage("detailed")}>Detailed Career Assessment</Button>
+          </div>
+        </Col>
+      </Row>
+      </div>
+      <Form className="api-input">
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" value={keyValue} onChange={changeKey} />
         <br />
@@ -21,8 +44,6 @@ export function HomePage({ changePage, changeKey, handleSubmit, keyValue }: Home
           Submit
         </Button>
       </Form>
-      <Button onClick={() => changePage("basic")}>Basic Career Assessment</Button>
-      <Button onClick={() => changePage("detailed")}>Detailed Career Assessment</Button>
     </div>
   );
 }
