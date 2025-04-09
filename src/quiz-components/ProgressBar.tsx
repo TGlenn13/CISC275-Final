536803ba5205fd8db24a9396ca61373ca3825ed8
magnus <magnus@udel.edu>
2025-04-09 11:14:28 -0400

    added a comment for later

--- src/quiz-components/ProgressBar.tsx
+++ src/quiz-components/ProgressBar.tsx
@@ -1,7 +1,7 @@
 import { ProgressBar } from "react-bootstrap"
 
 export function QuizProgressBar({progress}: {progress:number}): React.JSX.Element {
-    if (progress===8){
+    if (progress===8){ // Need to update if amount of questions change
         return(
             <ProgressBar now={progress} max={8} min={0} variant="success"></ProgressBar>
         )
