d473c5a6df3c883e8980ed750cadf48c7ba18a99
Ben Wootten <bwootten@udel.edu>
2025-04-10 15:37:58 -0400

    added buttons to scroll between pages

--- src/quiz-components/QuestionPage.tsx
+++ src/quiz-components/QuestionPage.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
-import { Row, Col } from "react-bootstrap"
+import { Row, Col, Button } from "react-bootstrap"
 
 export function QuestionPage({pages}: {pages:React.JSX.Element[][]}): React.JSX.Element {
+    /* pages is a 2d array of React.JSX.Elements, each of which is a question. The inner array
+    formats questions into groups, each of which is a page. The outer array represents the set of
+    all pages in the quiz*/
+    const [pageNum, setPageNum] = useState<number>(0);
+
+
+    // Formats sets of 4 questions from into rows and columns, creating a page element
+    const formatted: React.JSX.Element[] = pages.map((page: React.JSX.Element[]) => {
+        return(
+            <Row>
+                <Col>{page[0]}{page[2]}</Col>
+                <Col>{page[1]}{page[3]}</Col>
+            </Row>
+        )
+    })
+
     return (
         <div>
-            {pages.map((page: React.JSX.Element[]) => {
-                return(
-                    <Row>
-                        <Col>{page[0]}{page[2]}</Col>
-                        <Col>{page[1]}{page[3]}</Col>
-                    </Row>
-                )
-            })}
+            {formatted[pageNum]}
+            <Button disabled={pageNum <= 0} onClick={() => setPageNum(pageNum-1)}>Prev</Button>
+            <Button disabled={pageNum >= formatted.length-1} onClick={() => setPageNum(pageNum+1)}>Next</Button>
         </div>
     );
 }
