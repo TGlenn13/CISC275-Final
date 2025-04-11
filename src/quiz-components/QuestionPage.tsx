5a2656e26044d20b8090634a58af064a0cfdce0c
Ben Wootten <bwootten@udel.edu>
2025-04-11 11:30:52 -0400

    render MultipleChoiceQuestions in questionPage for persistence

--- src/quiz-components/QuestionPage.tsx
+++ src/quiz-components/QuestionPage.tsx
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
 import { Row, Col, Button } from "react-bootstrap"
+import { MultipleChoiceQuestion } from "./MultipleChoiceQuestion";
 
-export function QuestionPage({pages}: {pages:React.JSX.Element[][]}): React.JSX.Element {
-    /* pages is a 2d array of React.JSX.Elements, each of which is a question. The inner array
-    formats questions into groups, each of which is a page. The outer array represents the set of
-    all pages in the quiz*/
+interface Question {
+    question: string;
+    options: string[];
+    selectedAnswer: string;
+    onAnswerChange: (answer: string) => void;
+}
+
+export function QuestionPage({questionGroups}: {questionGroups: Question[][]}): React.JSX.Element {
+    // questionGroups is a 2d array of questions, grouped into 4 for each page
     const [pageNum, setPageNum] = useState<number>(0);
 
-
-    // Formats sets of 4 questions from into rows and columns, creating a page element
-    const formatted: React.JSX.Element[] = pages.map((page: React.JSX.Element[]) => {
-        return(
-            <Row>
-                <Col>{page[0]}{page[2]}</Col>
-                <Col>{page[1]}{page[3]}</Col>
-            </Row>
-        )
-    })
+    const currentPage=questionGroups[pageNum];
 
     return (
         <div>
-            {formatted[pageNum]}
+            <Row>
+                <Col><MultipleChoiceQuestion {...currentPage[0]}></MultipleChoiceQuestion></Col>
+                <Col><MultipleChoiceQuestion {...currentPage[1]}></MultipleChoiceQuestion></Col>
+            </Row>
+            <Row>
+                <Col><MultipleChoiceQuestion {...currentPage[2]}></MultipleChoiceQuestion></Col>
+                <Col><MultipleChoiceQuestion {...currentPage[3]}></MultipleChoiceQuestion></Col>
+            </Row>
             <Button disabled={pageNum <= 0} onClick={() => setPageNum(pageNum-1)}>Prev</Button>
-            <Button disabled={pageNum >= formatted.length-1} onClick={() => setPageNum(pageNum+1)}>Next</Button>
+            <Button disabled={pageNum >= questionGroups.length-1} onClick={() => setPageNum(pageNum+1)}>Next</Button>
         </div>
     );
 }
