aa18b5dc34f35bb9267942d69e1070c6c56ce6e5
Ben Wootten <bwootten@udel.edu>
2025-04-17 15:38:57 -0400

    reformatted to be compatible with Question interface

--- src/quiz-components/DetailedQuestions.tsx
+++ src/quiz-components/DetailedQuestions.tsx
@@ -1,16 +1,34 @@
-export type DetailedQuestion={
-    name:string;
-    label:string
-    completed:boolean;
-}
-
-export const detailedQuestions:DetailedQuestion[]=[
-    {name:"strengths",label:"Describe a time when you solved a complex problem or overcame a challenge. What approach did you take, and what did you learn about your strengths?",completed:false},
-    {name:"interests",label:"What are three topics or industries you find fascinating, even if you don’t have direct experience with them?",completed:false},
-    {name:"workEnvironment",label:"When you imagine your ideal work environment, what does it look and feel like?",completed:false},
-    {name:"preferredTasks",label:"What activities or tasks make you feel most engaged and in the zone?",completed:false},
-    {name:"idealWork",label:"If you could design your own job from scratch, what would it include? What tasks would you do daily, and what kind of outcomes would you aim for?",completed:false},
-    {name:"role",label:"How do you prefer to collaborate with others—leading a team, working independently, or supporting in a team role?",completed:false},
-    {name:"skills",label:"What skills or abilities come naturally to you, and how have you used them in past experiences—academic, personal, or professional?",completed:false},
-    {name:"values",label:"What values or causes are most important to you, and how would you like your work to reflect them?",completed:false}
+export const unformattedDetailedQuestions=[
+    {
+        name:"strengths",
+        question:"Describe a time when you solved a complex problem or overcame a challenge. What approach did you take, and what did you learn about your strengths?"
+    },
+    {
+        name:"interests",
+        question:"What are three topics or industries you find fascinating, even if you don’t have direct experience with them?"
+    },
+    {
+        name:"workEnvironment",
+        question:"When you imagine your ideal work environment, what does it look and feel like?"
+    },
+    {
+        name:"preferredTasks",
+        question:"What activities or tasks make you feel most engaged and in the zone?"
+    },
+    {
+        name:"idealWork",
+        question:"If you could design your own job from scratch, what would it include? What tasks would you do daily, and what kind of outcomes would you aim for?"
+    },
+    {
+        name:"role",
+        question:"How do you prefer to collaborate with others—leading a team, working independently, or supporting in a team role?"
+    },
+    {
+        name:"skills",
+        question:"What skills or abilities come naturally to you, and how have you used them in past experiences—academic, personal, or professional?"
+    },
+    {
+        name:"values",
+        question:"What values or causes are most important to you, and how would you like your work to reflect them?"
+    }
 ];
