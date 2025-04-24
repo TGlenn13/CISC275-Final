d83b22d84f06a9518758fdd9bfe63769df7a0d83
Ben Wootten <bwootten@udel.edu>
2025-04-24 15:59:39 -0400

    display promp result in page

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -2,8 +2,10 @@ import  React, {useState, useEffect} from "react";
 import OpenAI from "openai";
 import type { ChatCompletion } from "openai/resources/chat/completions";
 
-export function ResultsPage(): React.JSX.Element {
+export function ResultsPage({quizResponses}: {quizResponses: string}): React.JSX.Element {
     const [client, setClient] = useState<OpenAI | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [result, setResult] = useState<string>("");
 
     // Run once on mount
     useEffect(() => {updateKey();}, []);
@@ -13,28 +15,31 @@ export function ResultsPage(): React.JSX.Element {
         if (key) setClient(new OpenAI({apiKey: key ?? undefined, dangerouslyAllowBrowser: true}));
     }
 
-    async function prompt(): Promise<string> {
+    async function prompt(quizResults: string) {
         if (!client) throw new Error("API key invalid");
         console.log(client.apiKey);
-
+        
+        setLoading(true);
         const completion: ChatCompletion =
             await client.chat.completions.create({
                 model: "gpt-4.1-nano",
                 messages: [
                     {
                         role: "user",
-                        content: "Write a one-sentence bedtime story about a unicorn.",
+                        content: "Generate a report of recommended careers based on my answers to the following questiomnaire:\n" + quizResults,
                     },
                 ],
             });
         const response: string | null = completion.choices[0].message.content;
-        return response ? response : "";
+        setLoading(false);
+        setResult(response ? response : "");
     }
 
     return (
     <div>
         <h1>Results</h1>
-        <button onClick={async () => console.log(await prompt())}>prompt</button>
+        <button onClick={async () => console.log(await prompt(quizResponses))}>prompt</button>
+        {loading ? "loading" : result}
     </div>
     )
 }
