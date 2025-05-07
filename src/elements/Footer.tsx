c6039e009831900b958a3ab8d2f7ef95e384182e
Ben Wootten <bwootten@udel.edu>
2025-05-07 11:54:32 -0400

    attribution

--- src/elements/Footer.tsx
+++ src/elements/Footer.tsx
@@ -5,6 +5,8 @@ export function Footer(): React.JSX.Element{
     return(
         <div id="footer">
             Thomas Glenn, Benjamin Wootten, Magnus Culley
+            <br></br>
+            <a href="https://www.vecteezy.com/free-vector/blue-minimalist-background">Blue Minimalist Background Vectors by Vecteezy</a>
         </div>
     )
 }
