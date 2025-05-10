43f6b240961bec6835ddbd100a1dd2b5a04d1d2e
magnusculley <magnus@udel.edu>
2025-05-10 02:00:29 -0400

    changed favicon and web title

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -36,7 +36,7 @@ export function HomePage({ changePage, changeKey, handleSubmit, keyValue, error,
   return (
     <div className="HomePage">
       <div id="headerbox">
-        <div id="logo"></div>
+        <span id="logo"></span>
         <h1 id="title">Career Helpi</h1>
       </div>
       <Row>
