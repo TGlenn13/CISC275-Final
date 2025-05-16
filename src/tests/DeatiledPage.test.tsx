4ea6bbe350f71c78ebcc8a9ae89d7147f1d981e9
magnusculley <magnus@udel.edu>
2025-05-16 19:01:52 -0400

    deatiled test page is more comprehensive

--- src/tests/DeatiledPage.test.tsx
+++ src/tests/DeatiledPage.test.tsx
@@ -1,6 +1,9 @@
-import { render, screen} from '@testing-library/react';
+import { fireEvent, render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import {DetailedPage} from '../pages/DetailedPage';
+import userEvent from '@testing-library/user-event';
+
+
 
 describe('DetailedPage', () => {
     const mockProps = {
@@ -13,6 +16,25 @@ describe('DetailedPage', () => {
         setQuizResponses: jest.fn()
     };
 
+    async function finishQuiz() {
+        const textareasPage1 = document.querySelectorAll('textarea');
+
+        await userEvent.type(textareasPage1[0], 'coding');
+        await userEvent.type(textareasPage1[1], 'css');
+        await userEvent.type(textareasPage1[2], 'home');
+        await userEvent.type(textareasPage1[3], 'testing');
+
+        await userEvent.click(screen.getByRole('button', { name: /next/i }));
+
+        const textareasPage2 = document.querySelectorAll('textarea');
+
+        await userEvent.type(textareasPage2[0], 'quick');
+        await userEvent.type(textareasPage2[1], 'worker bee');
+        await userEvent.type(textareasPage2[2], 'testing');
+        await userEvent.type(textareasPage2[3], 'fun');
+    }
+
+
     test('renders header', () => {
         render(<DetailedPage {...mockProps} />);
         expect(screen.getByText('Detailed Career Assessment')).toBeInTheDocument();
@@ -21,12 +43,48 @@ describe('DetailedPage', () => {
 
     test('renders submit button', () => {
         render(<DetailedPage {...mockProps} />);
-        expect(screen.getByText('Submit')).toBeInTheDocument();
+        expect(screen.getByRole('button',{name:/Submit/i})).toBeInTheDocument();
     });
 
     test('submit disabled when quiz isnt finished', () => {
         render(<DetailedPage {...mockProps} />);
-        const submitButton = screen.getByText('Submit');
-        expect(submitButton).toBeDisabled();
+        expect(screen.getByRole('button',{name:/Submit/i})).toBeDisabled();
+    });
+
+    test('submit enabled when quiz is finished', async () => {
+        render(<DetailedPage {...mockProps} />);
+        await finishQuiz();
+        expect(screen.getByRole('button', { name: /submit/i })).toBeEnabled();
+    });
+
+    test('next button is enabled on page one', () => {
+        render(<DetailedPage {...mockProps} />);
+        expect(screen.getByRole('button', { name: /Next/i })).toBeEnabled();
+    });
+
+    test('next button is disabled on page two', () => {
+        render(<DetailedPage {...mockProps} />);
+        fireEvent.click(screen.getByRole('button', { name: /Next/i }));
+        expect(screen.getByRole('button', { name: /Next/i })).toBeDisabled();
+    });
+
+    test('back button is disabled on page one', () => {
+        render(<DetailedPage {...mockProps} />);
+        expect(screen.getByRole('button', { name: /Back/i })).toBeDisabled();
+    });
+
+    test('back button is enabled on page two', () => {
+        render(<DetailedPage {...mockProps} />);
+        fireEvent.click(screen.getByRole('button', { name: /Next/i }));
+        expect(screen.getByRole('button', { name: /Back/i })).toBeEnabled();
+    });
+
+    test('submission popup appears when quiz is submitted', async () => {
+        render(<DetailedPage {...mockProps} />);
+        await finishQuiz();
+        fireEvent.click(screen.getByRole('button', { name: /submit/i }));
+        expect(screen.getByText(/submission successful/i)).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: /show results/i })).toBeInTheDocument();
     });
 });
+
