463337f035b9e5eeb3f61782163d16d32ab17019
magnus <magnus@udel.edu>
2025-05-16 15:50:05 -0400

    added tets for submit button

--- src/tests/BasicPage.test.tsx
+++ src/tests/BasicPage.test.tsx
@@ -18,4 +18,23 @@ describe('BasicPage', () => {
         expect(screen.getByText('Basic Career Assessment')).toBeInTheDocument();
         expect(screen.getByRole('progressbar')).toBeInTheDocument();
     });
+
+    // test('renders first question page', () => {
+    //     render(<BasicPage {...mockProps} />);
+    //     expect(screen.getByText('1.')).toBeInTheDocument();
+    //     expect(screen.getByText('2.')).toBeInTheDocument();
+    //     expect(screen.getByText('3.')).toBeInTheDocument();
+    //     expect(screen.getByText('4.')).toBeInTheDocument();
+    // });
+
+    test('renders submit button', () => {
+        render(<BasicPage {...mockProps} />);
+        expect(screen.getByText('Submit')).toBeInTheDocument();
+    });
+
+    test('submit disabled when quiz isnt finished', () => {
+        render(<BasicPage {...mockProps} />);
+        const submitButton = screen.getByText('Submit');
+        expect(submitButton).toBeDisabled();
+    });
 });
