d2135aa5b3df93f13471f4bb17fc7329397fa52d
magnus <magnus@udel.edu>
2025-05-15 22:46:52 -0400

    added folder for tests

--- src/tests/HomePage.test.tsx
+++ src/tests/HomePage.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { HomePage } from "./HomePage";
+import { HomePage } from "../pages/HomePage";
 
 jest.mock("openai", () => ({
     OpenAI: jest.fn().mockImplementation(() => ({
